07c790c407cae8eed38e907d8fb4982a4201c4b9
Joey Allen <joey.allen761@gmail.com>
2018-01-02 19:57:53 -0500

    Add files via upload

---
+++ NBA_Salaries.py
@@ -0,0 +1,16 @@
+# Open up the file nbasalaries.csv
+salaries_file = open("nbasalaries.csv",'r')
+#{}=dictionary
+teams={}
+for line in salaries_file:
+    player_data = line.split(',')
+    player_team=player_data[1]
+    salary=int(player_data[2])
+    if player_team in teams.keys():
+        teams[player_team] += salary
+    else:
+        teams[player_team] = salary
+
+print teams.keys()
+for w in sorted(teams,key=teams.get,reverse=True):
+    print w, teams[w]
